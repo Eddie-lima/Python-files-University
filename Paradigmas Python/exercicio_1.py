ddd9d06445a11cf5ad0c3a9f38e472da63f1d8db
Ed <106930824+Eddie-lima@users.noreply.github.com>
2023-10-13 19:26:20 -0300

    Update exercicio_1.py

--- Paradigmas Python/exercicio_1.py
+++ Paradigmas Python/exercicio_1.py
@@ -1,8 +1,16 @@
 #29/08/23 - Paradigmas de Linguagens Python#
+def main():
+    try:  
+            horas_mes = int(input("Quantas horas foram trabalhadas neste mês? "))
+            valor_hora = float(input("Quanto vale uma hora trabalhada? "))
 
-horas_mes = int(input("Quantas horas foram trabalhadas neste mês? "))
-valor_hora = int(input("Quanto vale uma hora trabalhada? "))
-
-total = horas_mes * valor_hora
-
-print(total)
+            total = horas_mes * valor_hora
+            if horas_mes <= 672:
+                print("Você ganhou esse mês:", int(total))
+            else:
+                print("Vocẽ não pode trabalhar mains que 672 horas em um mês")
+                main()
+    except ValueError:
+        print("Insira um valor valido")
+        main()
+main()
